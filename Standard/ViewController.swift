31b6b824a9d842b53f01bdc432598a4dbc26d171
文优 <yi.li@hipac.cn>
2020-01-13 16:27:03 +0800

    Initial Commit

---
+++ Standard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standard
+//
+//  Created by 李毅 on 2020/1/13.
+//  Copyright © 2020 文优. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
